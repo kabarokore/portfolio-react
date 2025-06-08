65f371aa5ca19150cc732185d616eb9fed6f68c3
kabarokore <kabarokorejesco@gmail.com>
2025-06-08 20:43:20 +0200

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Hero from './components/Hero';
-import About from './components/about';
+import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
